a3c9fe7aab95231a763c163f7de97e2e5af25015
mahdiandaz <mehdiandaz825@gmail.com>
2025-10-30 21:01:22 +0100

    Add h2/H2_main (min/max over i,j,k under pairwise distinct)

---
+++ src/h2/H2_main.java
@@ -0,0 +1,20 @@
+package h2;
+
+public class H2_main {
+    public static void main(String[] args) {
+        int i = 7;
+        int j = 3;
+        int k = 11;
+
+        int min = i;
+        if (j < min) min = j;
+        if (k < min) min = k;
+
+        int max = i;
+        if (j > max) max = j;
+        if (k > max) max = k;
+
+        System.out.println("min = " + min);
+        System.out.println("max = " + max);
+    }
+}
